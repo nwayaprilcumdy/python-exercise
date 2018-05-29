5dfd7110abda2b33ca7190d90e0779dc636ad9f6
nwayaprilcumdy <nwayapril97@gmail.com>
2018-05-30 02:48:40 +0630

    update my ex14th exercise

--- ex14.py
+++ ex14.py
@@ -2,7 +2,7 @@ from sys import argv
 script, user_name = argv
 prompt = '> '
 print(f"Hi {user_name}, I'm the {script} script.")
-print(f"I'd like to ask you a few questions.")
+print("I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
 likes = input(prompt)
 print(f"Where do you live {user_name}?")
