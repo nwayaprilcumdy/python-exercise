4cc6dfcb02d9fa3636703ea8627b9c8a54a11e15
nwayaprilcumdy <nwayapril97@gmail.com>
2018-06-25 19:47:45 -0700

    change dog to cat

--- ex_101.py
+++ ex_101.py
@@ -1,16 +1,16 @@
-tabby_red = "\tI'm tabbed in."
-persian_red = "I'm split\non a line."
-backslash_red = "I'm \\ a \\ red."
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
 
 fat_red = """
 I'll do a list:
-\t* Red food
+\t* Cat food
 \t* Fishies
 \t* Catnip\n\t*Grass
 """
 
-print(tabby_red)
-print(persian_red)
-print(backslash_red)
-print(fat_red)
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
 
