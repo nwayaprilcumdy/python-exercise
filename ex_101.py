aac66b94cc19442ce360fb75dec292c1793569b4
nwayaprilcumdy <39269246+nwayaprilcumdy@users.noreply.github.com>
2018-06-25 19:51:01 -0700

    Change cat to rap

--- ex_101.py
+++ ex_101.py
@@ -1,16 +1,16 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
+tabby_rap = "\tI'm tabbed in."
+persian_rap = "I'm split\non a line."
+backslash_rap = "I'm \\ a \\ cat."
 
-fat_red = """
+fat_rap = """
 I'll do a list:
-\t* Cat food
+\t* Rap food
 \t* Fishies
 \t* Catnip\n\t*Grass
 """
 
-print(tabby_cat)
-print(persian_cat)
-print(backslash_cat)
-print(fat_cat)
+print(tabby_rap)
+print(persian_rap)
+print(backslash_rap)
+print(fat_rap)
 
